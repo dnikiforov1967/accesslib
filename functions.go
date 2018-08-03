47f41fa5906548399041771193969c4306460d16
Dmitry Nikiforov <dmitry.nikiforov@adition.com>
2018-08-03 10:48:51 +0200

    Implement read-write based thread safe access to limit data

--- functions.go
+++ functions.go
@@ -1,21 +1,31 @@
 package accesslib
 
-import "sync"
 import "time"
 import "fmt"
 
-var lock sync.Mutex
+func ReadLimit(clientId string) (int64, bool) {
+	limitMutex.RLock()
+	defer limitMutex.RUnlock()
+	limit, ok := ClientLimits[clientId]
+	return limit, ok
+}
+
+func WriteLimit(clientId string, limit int64) {
+	limitMutex.Lock()
+	defer limitMutex.Unlock()
+	ClientLimits[clientId] = limit
+}
 
 //Access rate controller should be defended by mutex (at least if we want to
 //implement lazy initialization
 func AccessRateControl(clientId string) bool {
-        limit, ok := ClientLimits[clientId]
+        limit, ok := ReadLimit(clientId)
         returnValue := true
         if !ok {
             returnValue = false
         } else {
-            lock.Lock();
-            defer lock.Unlock()
+            rateMutex.Lock();
+            defer rateMutex.Unlock()
             val, ok := rateLimitMap[clientId]	
             if !ok {
 		fmt.Printf("We initiate rate limit for %s\n",clientId);
--- variables.go
+++ variables.go
@@ -1,4 +1,9 @@
 package accesslib
 
+import "sync"
+
+var limitMutex sync.RWMutex
+var rateMutex sync.Mutex 
+
 var rateLimitMap map[string]*accessTrackingStruct = make(map[string]*accessTrackingStruct)
 var ClientLimits map[string]int64 = make(map[string]int64)
